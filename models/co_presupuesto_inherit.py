3b2f08fae39680cd3972780ba3b2059fc63b3500
Juan Camilo Zuluaga Serna <juancamilozuluagaserna@hotmail.com>
2018-08-18 14:15:32 -0500

    agregando cambio para que solo carguen los documentos que tienen saldo

--- models/co_presupuesto_inherit.py
+++ models/co_presupuesto_inherit.py
@@ -154,8 +154,6 @@ class presupuesto_move_inherit(models.Model):
 	@api.onchange('presupuesto_rel_move')
 	def _onchange_rel_move_ids(self):
 
-		_logger.info(self._origin.id)
-
 		if self.presupuesto_rel_move:
 
 			rpre_moverubros = self.env['presupuesto.moverubros']
@@ -169,6 +167,50 @@ class presupuesto_move_inherit(models.Model):
 			self.gastos_ids = lista_rubros
 		else:
 			self.gastos_ids=None
+
+
+
+
+
+	@api.onchange('date')
+	def onchange_date(self):
+
+		doc_type = {'reg' : 'cdp', 'obl': 'reg', 'pago': 'obl'}
+
+		presupuesto_tools = self.env['presupuesto.tools']
+		presupuesto_rel_move = []
+
+		if self.date:
+			period = self.env['account.period'].find(self.date)
+			self.period_id = period.id
+			self.fiscal_year = period.fiscalyear_id.id
+
+			if self.fiscal_year and 'default_doc_type' in self.env.context:
+
+				presupuesto_rel_move_ids = self.search([('fiscal_year', '=', self.fiscal_year.id), 
+						('doc_type', '=', doc_type.get(self.env.context.get('default_doc_type')))])
+
+				if presupuesto_rel_move_ids:
+
+					for x in presupuesto_rel_move_ids:
+
+						if presupuesto_tools.get_saldo(x) > 0:
+
+							presupuesto_rel_move.append(x.id)
+							
+					_logger.info(presupuesto_rel_move)
+					
+					return {'domain': {'presupuesto_rel_move': [('id', 'in', (presupuesto_rel_move))]}}
+
+
+
+
+		if not self.date and self.fiscal_year:
+			period = self.env['account.period'].find(self.date)
+			self.period_id = period.id
+			self.date = self.fiscal_year.date_start
+			self.date_start = self.fiscal_year.date_start
+			self.date_stop = self.fiscal_year.date_stop
 
 
 
