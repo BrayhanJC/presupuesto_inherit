ac9ab207ee8ef6a156bc2022ffc96e94d4ec4bc4
camilozuluaga <juancamilozuluagaserna@hotmail.com>
2019-01-04 14:50:24 -0500

    agregando cambio para la suma del dinero que se libera del cdp

--- models/co_presupuesto_moverubros_inherit.py
+++ models/co_presupuesto_moverubros_inherit.py
@@ -155,14 +155,13 @@ class presupuesto_moverubros_inherit(models.Model):
 			move_saldo = saldo_resta = saldo_suma = 0.0
 			for move in ids:
 				if move.mov_type == 'ini' or move.mov_type == 'adi' or move.mov_type == 'cre':
-
-					if move.mov_type == 'lcdp' and move.move_id.state == 'confirm':
-						saldo_suma += move.ammount
-					else:
-						saldo_suma += move.ammount
+					
+					saldo_suma += move.ammount
 
 				if move.mov_type == 'red' or move.mov_type == 'cont' or move.mov_type == 'cdp' or move.mov_type == 'rec':
 					saldo_resta += move.ammount
+					if move.mov_type == 'lcdp' and move.move_id.state == 'confirm':
+						saldo_suma += move.ammount
 				move_saldo = (saldo_suma - saldo_resta) if saldo_suma else saldo_resta
 
 			self.saldo_move = move_saldo
